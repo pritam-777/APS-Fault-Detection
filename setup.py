e40cb5ae47a5fa86dda796647c7ad3670320ea3f
pritam-777 <pritambhakta90@gmail.com>
2022-11-30 01:09:20 +0530

    update new code

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import find_packages,setup
+from typing import List
 
-def get_requirments():
-    pass 
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements()->List[str]:
+    
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
 
 setup(
     name="sensor", 
@@ -9,5 +20,5 @@ setup(
     author= "pritam bhakta",
     author_email="pritambhakta90@gmail.com",
     packages=find_packages(),
-    install_required = get_requirments()
+    install_required = get_requirements()
 )
